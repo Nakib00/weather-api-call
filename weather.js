e9825c07ba7c25094c431aa9c12b737954fc27a1
Nakib00 <2022142@iub.edu.bd>
2023-04-13 22:24:34 +0600

    add new

--- weather.js
+++ weather.js
@@ -1,5 +1,6 @@
 function connect() {
   var userText = document.getElementById("user-input").value;
+  var userText = "Feni"
 
   var url = `https://api.openweathermap.org/data/2.5/weather?q=${userText}&appid=8574c3e9bd8bc148b2516404e61e931a`;
 
@@ -11,10 +12,20 @@ function connect() {
 function show(data) {
   var city_name = data["name"];
   var temp_k = data["main"]["temp"];
-  var temo_c = temp_k - 273.15;
+  var temo_l = temp_k - 273.15;
+  var temp_c = temo_l.toFixed(2)
   var country = data["sys"]["country"];
+  var humidity = data["main"]["humidity"];
+  var wind = data["wind"]["speed"];
+  var feel_like_t = data["main"]["feels_like"];
+  var feel_like_k = feel_like_t - 273.15
+  var feel_like = feel_like_k.toFixed(2)
 
-  document.getElementById(
-    "contaner"
-  ).innerHTML = `<h1>Country Name: ${country}</h1> <h1>City Name: ${city_name}</h1> <h1>Temp: ${temo_c} C</h1>`;
+
+  document.getElementById("tamp").innerHTML = temp_c;
+  document.getElementById("humi").innerHTML = humidity;
+  document.getElementById("wind").innerHTML = wind;
+  document.getElementById("city").innerHTML = city_name;
+  document.getElementById("feel_like").innerHTML = feel_like;
+
 }
