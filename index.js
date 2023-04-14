7a24102f2f032cb3170cf7d1760eed39afe20e46
Nakib00 <2022142@iub.edu.bd>
2023-04-15 00:21:09 +0600

    dd

--- index.js
+++ index.js
@@ -1,6 +1,15 @@
+// Default city
+var userText = "Feni"
+
+var url = `https://api.openweathermap.org/data/2.5/weather?q=${userText}&appid=8574c3e9bd8bc148b2516404e61e931a`;
+
+  fetch(url)
+    .then((res) => res.json())
+    .then((data) => show(data));
+
+// Search cities
 function connect() {
   var userText = document.getElementById("user-input").value;
-  // var userText = "Feni"
 
   var url = `https://api.openweathermap.org/data/2.5/weather?q=${userText}&appid=8574c3e9bd8bc148b2516404e61e931a`;
 
@@ -9,23 +18,30 @@ function connect() {
     .then((data) => show(data));
 }
 
+// Show weather data
 function show(data) {
   var city_name = data["name"];
   var temp_k = data["main"]["temp"];
-  var temo_l = temp_k - 273.15;
-  var temp_c = temo_l.toFixed(2)
+  var temp_c = (temp_k - 273.15).toFixed(2);
   var country = data["sys"]["country"];
   var humidity = data["main"]["humidity"];
   var wind = data["wind"]["speed"];
   var feel_like_t = data["main"]["feels_like"];
-  var feel_like_k = feel_like_t - 273.15
-  var feel_like = feel_like_k.toFixed(2)
+  var feel_like = (feel_like_t - 273.15).toFixed(2)
+  // var temp_min_k = data["main"]["temp_min"];
+  // var temp_min = (temp_min_k - 273.15).toFixed(2)
+  // var temp_max_k = data["main"]["temp_max"];
+  // var temp_max = (temp_min_k - 273.15).toFixed(2)
+  var pressure = data["main"]["pressure"];
+  var deg = data["wind"]["deg"];
 
-
+// show data in the html element
   document.getElementById("tamp").innerHTML = temp_c;
   document.getElementById("humi").innerHTML = humidity;
   document.getElementById("wind").innerHTML = wind;
   document.getElementById("city").innerHTML = city_name;
   document.getElementById("feel_like").innerHTML = feel_like;
+  document.getElementById("pressure").innerHTML = pressure;
+  document.getElementById("deg").innerHTML = deg;
 
 }
