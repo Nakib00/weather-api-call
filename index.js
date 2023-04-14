1494bfc50d13e86ae41ba70ec9e0bd8f28ed069c
Nakib00 <2022142@iub.edu.bd>
2023-04-15 01:52:17 +0600

    new icone

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ function show(data) {
   var city_name = data["name"];
   var temp_k = data["main"]["temp"];
   var temp_c = (temp_k - 273.15).toFixed(2);
-  var country = data["sys"]["country"];
+  var country = data["weather"]["icon"];
   var humidity = data["main"]["humidity"];
   var wind = data["wind"]["speed"];
   var feel_like_t = data["main"]["feels_like"];
@@ -35,11 +35,20 @@ function show(data) {
   var pressure = data["main"]["pressure"];
   var deg = data["wind"]["deg"];
 
+  var icone_array = ["01d","01n","02d","02n","03d","03n","04d","04n","09d","09n","10d","10n","11d","11n","13d","13n","50d","50n","1232n"];
+  var iconecode
+
+  for(var i = 0; i<icone_array.length; i++) {
+    if(icone_array[i] == iconecode){
+
+    }
+  }
+
 // show data in the html element
   document.getElementById("tamp").innerHTML = temp_c;
   document.getElementById("humi").innerHTML = humidity;
   document.getElementById("wind").innerHTML = wind;
-  document.getElementById("city").innerHTML = city_name;
+  document.getElementById("city").innerHTML = country;
   document.getElementById("feel_like").innerHTML = feel_like;
   document.getElementById("pressure").innerHTML = pressure;
   document.getElementById("deg").innerHTML = deg;
